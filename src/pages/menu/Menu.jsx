7909176ea831bfdf4a185a98841b937d9549f86f
yxiang921 <xiangf35@gmail.com>
2024-02-29 23:07:11 +0800

    Complete Page Design: Home, Order, Menu

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { MdOutlineEdit } from "react-icons/md";
+
+const FoodCard = ({ imagePath, name, price}) => {
+  return (
+    <div className="w-52 h-52 bg-white rounded-lg shadow-lg m-4 flex flex-col justify-center items-center relative">
+      <div className="rounded-full w-24 h-24 bg-base-200"></div>
+      <h1 className="text-lg font-bold text-neutral-600 pt-2">{name}</h1>
+      <span className="font-medium text-neutral-400">RM{price}</span>
+
+      <div className="w-8 h-8 bg-base-300 rounded-full absolute top-4 right-4 cursor-pointer flex justify-center items-center">
+        <MdOutlineEdit />
+      </div>
+    </div>
+  );
+};
 
 const Menu = () => {
   return (
-    <div>Menu</div>
-  )
-}
+    <div className="w-full flex flex-col justify-center items-center p-6">
+      <div className="rounded-lg w-full overflow-x-auto">
+        <div className="w-full h-12 flex flex-row justify-between items-center ">
+          <h1 className="text-lg font-bold text-neutral-700">Current Menu</h1>
+          <button className="btn bg-orange-color text-white">Add New</button>
+        </div>
+      </div>
+      <div className="w-full h-[35rem] mt-6 flex flex-row justify-start items-start flex-wrap">
+        {/* each food card (start) */}
+        <FoodCard name={"Chicken Sandwich"} price={9.00}/>
+        <FoodCard name={"Chicken Sandwich"} price={9.00}/>
+        <FoodCard name={"Chicken Sandwich"} price={9.00}/>
+        <FoodCard name={"Chicken Sandwich"} price={9.00}/>
+        <FoodCard name={"Chicken Sandwich"} price={9.00}/>
+        {/* each food card (end) */}
+      </div>
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
--- src/pages/order/Order.jsx
+++ src/pages/order/Order.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
-
 const Order = () => {
   return (
-    <div>Order</div>
-  )
-}
+    <div className="w-full h-screen flex justify-center items-center p-6">
+      <div className="bg-white rounded-lg shadow-md w-full h-[40rem]">
+        <h1 className="px-10 pt-4 text-lg font-medium text-neutral-700">Order Management</h1>
+        {/* table start */}
 
-export default Order
+        <div className="overflow-x-auto m-6">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>
+                  <label>
+                    <input type="checkbox" className="checkbox" />
+                  </label>
+                </th>
+                <th>Order Number</th>
+                <th>Table Number</th>
+                <th>Orders</th>
+                <th>Status</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <th>
+                  <label>
+                    <input type="checkbox" className="checkbox" />
+                  </label>
+                </th>
+                <td>
+                  <div className="flex items-center">
+                    <div>
+                      <div className="font-bold">#ORD123</div>
+                      <div className="text-sm opacity-50">12.34pm</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <div className="font-bold">#5</div>
+                </td>
+                <td className="w-1/4">
+                  <span>5 Items</span>
+                </td>
+                <td>
+                  <div className="bg-yellow-300 bg-opacity-55 h-8 rounded-md flex justify-center items-center">
+                    <span className="font-medium text-yellow-700">
+                      Processing
+                    </span>
+                  </div>
+                </td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        {/* table end */}
+      </div>
+    </div>
+  );
+};
+
+export default Order;
